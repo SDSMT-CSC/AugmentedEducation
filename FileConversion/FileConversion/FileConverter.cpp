5836d4eff59902795a002bb9495ab3966d61b76f
Kenneth Petry <kpetry@bresnan.net>
2017-11-14 11:40:34 -0700

    Added some logic to determine when to use the fbx and when to use the assimp utilities.

--- FileConversion/FileConversion/FileConverter.cpp
+++ FileConversion/FileConversion/FileConverter.cpp
@@ -1,4 +1,5 @@
 #include "FileConverter.h"
+#include <iostream>
 
 FileConverter::FileConverter()
 {
@@ -24,6 +25,28 @@ bool FileConverter::SupportsOutputFileType(std::string fileType)
 
 FileConverter::Result FileConverter::ConvertFile(std::string inputFilename, std::string outputFilename)
 {
-	return assimp.ConvertFile(inputFilename, outputFilename);
-	//return fbx.ConvertFile(inputFilename, outputFilename);
+	std::string inputFileType = this->ExtractFileExtention(inputFilename);
+	std::string outputFileType = this->ExtractFileExtention(outputFilename);
+
+	if (assimp.SupportsInputFileType(inputFileType) && assimp.SupportsOutputFileType(outputFileType))
+	{
+		return assimp.ConvertFile(inputFilename, outputFilename);
+	}
+	else if (fbx.SupportsInputFileType(inputFileType) && fbx.SupportsOutputFileType(outputFileType))
+	{
+		return fbx.ConvertFile(inputFilename, outputFilename);
+	}
+	else if (assimp.SupportsInputFileType(inputFileType) && fbx.SupportsOutputFileType(outputFileType))
+	{
+		std::string tempFilename = inputFilename.substr(0, inputFilename.rfind("."));
+		tempFilename = tempFilename + ".dae";
+
+		Result converionResult;
+		converionResult = assimp.ConvertFile(inputFilename, tempFilename);
+
+		if (converionResult < 0)
+			return converionResult;
+
+		return fbx.ConvertFile(tempFilename, outputFilename);
+	}
 }
