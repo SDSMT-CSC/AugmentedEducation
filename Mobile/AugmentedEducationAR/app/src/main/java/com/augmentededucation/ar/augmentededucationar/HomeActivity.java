1cac0eacdabc101561565b3ef0742831bbcbfa99
Kenneth Petry <kpetry@bresnan.net>
2018-01-24 11:22:48 -0700

    Adding buttons to homeactivity to move the focus to the ar activity.

--- Mobile/AugmentedEducationAR/app/src/main/java/com/augmentededucation/ar/augmentededucationar/HomeActivity.java
+++ Mobile/AugmentedEducationAR/app/src/main/java/com/augmentededucation/ar/augmentededucationar/HomeActivity.java
@@ -1,7 +1,9 @@
 package com.augmentededucation.ar.augmentededucationar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 
 public class HomeActivity extends AppCompatActivity
@@ -10,5 +12,11 @@ public class HomeActivity extends AppCompatActivity
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home);
+	}
+
+	public void ViewInAR(View view)
+	{
+		Intent intent = new Intent(this, ARActivity.class);
+		startActivity(intent);
 	}
 }
