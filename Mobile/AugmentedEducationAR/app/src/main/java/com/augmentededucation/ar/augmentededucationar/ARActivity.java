a98470f6f76c3b2ce0a5cc7c194f366c22d42b72
Cheldon Coughlen <cheldon.coughlen@mines.sdsmt.edu>
2018-01-25 10:50:01 -0700

    Ported over most of HelloAR code

--- Mobile/AugmentedEducationAR/app/src/main/java/com/augmentededucation/ar/augmentededucationar/ARActivity.java
+++ Mobile/AugmentedEducationAR/app/src/main/java/com/augmentededucation/ar/augmentededucationar/ARActivity.java
@@ -1,19 +1,36 @@
 package com.augmentededucation.ar.augmentededucationar;
 
+import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Toast;
 
 import com.augmentededucation.ar.augmentededucationar.arcore.CameraPermissionHelper;
 import com.augmentededucation.ar.augmentededucationar.arcore.DisplayRotationHelper;
+import com.augmentededucation.ar.augmentededucationar.arcore.rendering.BackgroundRenderer;
+import com.augmentededucation.ar.augmentededucationar.arcore.rendering.ObjectRenderer;
+import com.augmentededucation.ar.augmentededucationar.arcore.rendering.PlaneRenderer;
+import com.augmentededucation.ar.augmentededucationar.arcore.rendering.PointCloudRenderer;
+import com.google.ar.core.Anchor;
+import com.google.ar.core.Camera;
 import com.google.ar.core.Config;
+import com.google.ar.core.Frame;
+import com.google.ar.core.HitResult;
+import com.google.ar.core.Plane;
+import com.google.ar.core.PointCloud;
 import com.google.ar.core.Session;
+import com.google.ar.core.Trackable;
 import com.google.ar.core.exceptions.UnavailableApkTooOldException;
 import com.google.ar.core.exceptions.UnavailableArcoreNotInstalledException;
 import com.google.ar.core.exceptions.UnavailableSdkTooOldException;
+
+import java.io.IOException;
+import java.util.concurrent.ArrayBlockingQueue;
 
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
@@ -24,7 +41,23 @@ public class ARActivity extends AppCompatActivity implements GLSurfaceView.Rende
 
     private GLSurfaceView surfaceView;
     private Session session;
+    private GestureDetector gestureDetector;
     private DisplayRotationHelper displayRotationHelper;
+
+    private final BackgroundRenderer backgroundRenderer = new BackgroundRenderer();
+    private final ObjectRenderer virtualObject = new ObjectRenderer();
+    private final PlaneRenderer planeRenderer = new PlaneRenderer();
+    private final PointCloudRenderer pointCloudRenderer = new PointCloudRenderer();
+
+    private Anchor anchor;
+    private final float[] anchorMatrix = new float[16];
+
+    private float scaleFactor = .1f;
+
+    private final ArrayBlockingQueue<MotionEvent> queuedSingleTaps = new ArrayBlockingQueue<MotionEvent>(16);
+
+    private String objectFileName;
+    private String materialFileName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +67,25 @@ public class ARActivity extends AppCompatActivity implements GLSurfaceView.Rende
         displayRotationHelper = new DisplayRotationHelper(this);
 
         // TODO: Add tap listener
+        gestureDetector= new GestureDetector(this, new GestureDetector.SimpleOnGestureListener() {
+            @Override
+            public boolean onSingleTapUp(MotionEvent e) {
+                onSingleTap(e);
+                return true;
+            }
+
+            @Override
+            public boolean onDown(MotionEvent e) {
+                return true;
+            }
+        });
+
+        surfaceView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return gestureDetector.onTouchEvent(event);
+            }
+        });
 
         // Set up renderer
         surfaceView.setPreserveEGLContextOnPause(true);
@@ -73,21 +125,107 @@ public class ARActivity extends AppCompatActivity implements GLSurfaceView.Rende
             Toast.makeText(this, "This device does not support AR", Toast.LENGTH_SHORT).show();
         }
         session.configure(config);
+
+        // TODO remove default file names
+        objectFileName = "Car.obj";
+        materialFileName = "Car.obj.mtl";
+    }
+
+    private void onSingleTap(MotionEvent e) {
+        queuedSingleTaps.offer(e);
     }
 
     @Override
     public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        GLES20.glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 
+        // Create the texture
+        backgroundRenderer.createOnGlThread(this);
+        if (session != null) {
+            session.setCameraTextureName(backgroundRenderer.getTextureId());
+        }
+
+        // This is where the model is loaded
+        try {
+            virtualObject.createOnGlThread(this, objectFileName, materialFileName);
+            virtualObject.setMaterialProperties(0.0f, 3.5f, 1.0f, 6.0f);
+        } catch (IOException e) {
+            Log.e(TAG, "Failed to read obj file or material");
+        }
+        try {
+            planeRenderer.createOnGlThread(this, "trigrid.png");
+        } catch (IOException e) {
+            Log.e(TAG, "Failed to read plane texture");
+        }
+        pointCloudRenderer.createOnGlThread(this);
     }
 
     @Override
     public void onSurfaceChanged(GL10 gl, int width, int height) {
-
+        displayRotationHelper.onSurfaceChanged(width, height);
+        GLES20.glViewport(0, 0, width, height);
     }
 
     @Override
     public void onDrawFrame(GL10 gl) {
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
 
+        if (session == null) {
+            return;
+        }
+
+        displayRotationHelper.updateSessionIfNeeded(session);
+
+        try {
+            Frame frame = session.update();
+            Camera camera = frame.getCamera();
+
+            // TODO MotionEvent tap
+            MotionEvent tap = queuedSingleTaps.poll();
+            if (tap != null && camera.getTrackingState() == Trackable.TrackingState.TRACKING) {
+                for (HitResult hit : frame.hitTest(tap)) {
+                    Trackable trackable = hit.getTrackable();
+                    if (trackable instanceof  Plane && ((Plane) trackable).isPoseInPolygon(hit.getHitPose())) {
+                        anchor = hit.createAnchor();
+                    }
+                }
+            }
+
+            backgroundRenderer.draw(frame);
+
+            if (camera.getTrackingState() == Trackable.TrackingState.PAUSED) {
+                return;
+            }
+
+            float[] projmtx = new float[16];
+            camera.getProjectionMatrix(projmtx, 0, 0.1f, 100.0f);
+
+            float[] viewmtx = new float[16];
+            camera.getViewMatrix(viewmtx, 0);
+
+            final float lightIntensity = frame.getLightEstimate().getPixelIntensity();
+
+            PointCloud pointCloud = frame.acquirePointCloud();
+            pointCloudRenderer.update(pointCloud);
+            pointCloudRenderer.draw(viewmtx, projmtx);
+
+            pointCloud.release();
+
+            // Visualize planes
+            planeRenderer.drawPlanes(
+                    session.getAllTrackables(Plane.class), camera.getDisplayOrientedPose(), projmtx);
+
+            //if (anchor != null) {
+                if (anchor.getTrackingState() == Trackable.TrackingState.TRACKING) {
+                    anchor.getPose().toMatrix(anchorMatrix, 0);
+
+                    virtualObject.updateModelMatrix(anchorMatrix, scaleFactor);
+                    virtualObject.draw(viewmtx, projmtx, lightIntensity);
+                }
+            //}
+        } catch (Throwable t) {
+            Log.e(TAG, "Exception on the OpenGL thread", t);
+        }
     }
 
     @Override
@@ -116,10 +254,13 @@ public class ARActivity extends AppCompatActivity implements GLSurfaceView.Rende
     }
 
     public void onBig(View v) {
-
+        scaleFactor += 0.025;
     }
 
     public void onLittle(View v) {
-
+        scaleFactor -= .025f;
+        if (scaleFactor < 0.01f) {
+            scaleFactor = 0.01f;
+        }
     }
 }
