5c00a47e3a59889570458d7a4db2fcca1db5e628
Cheldon Coughlen <cheldon.coughlen@mines.sdsmt.edu>
2018-01-25 09:44:53 -0700

    Filled out onCreate, onPause, onResume and other basic methods in ARActivity

--- Mobile/AugmentedEducationAR/app/src/main/java/com/augmentededucation/ar/augmentededucationar/ARActivity.java
+++ Mobile/AugmentedEducationAR/app/src/main/java/com/augmentededucation/ar/augmentededucationar/ARActivity.java
@@ -1,15 +1,118 @@
 package com.augmentededucation.ar.augmentededucationar;
 
-import android.support.v7.app.AppCompatActivity;
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
-public class ARActivity extends AppCompatActivity {
+import com.augmentededucation.ar.augmentededucationar.arcore.CameraPermissionHelper;
+import com.augmentededucation.ar.augmentededucationar.arcore.DisplayRotationHelper;
+import com.google.ar.core.Config;
+import com.google.ar.core.Session;
+import com.google.ar.core.exceptions.UnavailableApkTooOldException;
+import com.google.ar.core.exceptions.UnavailableArcoreNotInstalledException;
+import com.google.ar.core.exceptions.UnavailableSdkTooOldException;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+public class ARActivity extends AppCompatActivity implements GLSurfaceView.Renderer {
+    private static final String TAG = ARActivity.class.getSimpleName();
+
+
+    private GLSurfaceView surfaceView;
+    private Session session;
+    private DisplayRotationHelper displayRotationHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ar);
+        surfaceView = findViewById(R.id.surfaceview);
+        displayRotationHelper = new DisplayRotationHelper(this);
+
+        // TODO: Add tap listener
+
+        // Set up renderer
+        surfaceView.setPreserveEGLContextOnPause(true);
+        surfaceView.setEGLContextClientVersion(2);
+        surfaceView.setEGLConfigChooser(8, 8, 8, 8, 16, 0);
+        surfaceView.setRenderer(this);
+        surfaceView.setRenderMode(GLSurfaceView.RENDERMODE_CONTINUOUSLY);
+
+        // Create session
+        Exception exception = null;
+        String message = null;
+        try {
+            session = new Session(/* context= */ this);
+        } catch (UnavailableArcoreNotInstalledException e) {
+            message = "Please install ARCore";
+            exception = e;
+        } catch (UnavailableApkTooOldException e) {
+            message = "Please update ARCore";
+            exception = e;
+        } catch (UnavailableSdkTooOldException e) {
+            message = "Please update this app";
+            exception = e;
+        } catch (Exception e) {
+            message = "This device does not support AR";
+            exception = e;
+        }
+
+        if (message != null) {
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+            Log.e(TAG, "Exception creating session", exception);
+            return;
+        }
+
+        // Create default config and check if supported.
+        Config config = new Config(session);
+        if (!session.isSupported(config)) {
+            Toast.makeText(this, "This device does not support AR", Toast.LENGTH_SHORT).show();
+        }
+        session.configure(config);
+    }
+
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl) {
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (CameraPermissionHelper.hasCameraPermission(this)) {
+            if (session != null) {
+                session.resume();
+            }
+            surfaceView.onResume();
+            displayRotationHelper.onResume();
+        } else {
+            CameraPermissionHelper.requestCameraPermission(this);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        displayRotationHelper.onPause();
+        surfaceView.onPause();
+        if (session != null) {
+            session.pause();
+        }
     }
 
     public void onBig(View v) {
