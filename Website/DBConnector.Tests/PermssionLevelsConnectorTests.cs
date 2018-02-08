d04184d3a5d41274500601a4995a89c38d59e897
Shimperial <brady.shimp@mines.sdsmt.edu>
2018-02-08 11:24:28 -0700

    Able to call Create/Drop test tables in BaseConnectorTests from ClassInit/ClassTeardown in other test classes.

--- Website/DBConnector.Tests/PermssionLevelsConnectorTests.cs
+++ Website/DBConnector.Tests/PermssionLevelsConnectorTests.cs
@@ -17,76 +17,21 @@ namespace DBConnector.Tests
             public Test_PermissionLevelsConnecector() { _Table_Name = $"Test_{_Table_Name}"; }
         }
 
-        /*
+        [ClassInitialize]
+        public static void ClassInit(TestContext context)
+        { BaseConnectorTests.Create_Test_Tables(); }
+
+        [ClassCleanup]
+        public static void ClassTeardown()
+        { BaseConnectorTests.Drop_Test_Tables(); }
+
+
         [TestMethod]
         public void Test_Passing_All_Simple_Operations()
         {
-            PermissionLevel permission, from_query;
-            List<Fields> select_fields = new List<Fields>();
-            ResultPackage<bool> bool_result = new ResultPackage<bool>();
-            ResultPackage<string> string_result = new ResultPackage<string>();
-            Dictionary<Fields, object> set_fields = new Dictionary<Fields, object>();
-            PermissionLevelsConnector permissionConnector = new PermissionLevelsConnector();
-            ResultPackage<List<PermissionLevel>> query_result = new ResultPackage<List<PermissionLevel>>();
-
-            permission = new PermissionLevel(1, "default");
-            //Adding a comment to see if my commits are working at all...
-            //Insert user record
-            bool_result = Simple_Insert(permissionConnector, permission);
-            Assert.IsTrue(bool_result.ReturnValue, bool_result.ErrorMessage);
-
-
-            //Query for insert
-            string_result = Simple_Query_By_Id(permissionConnector, 1, new List<Fields>());
-            Assert.IsTrue(string.IsNullOrEmpty(string_result.ErrorMessage));
-            //Parse query and verify insert
-            query_result = Parse_Query(string_result.ReturnValue);
-            Assert.IsTrue(string.IsNullOrEmpty(query_result.ErrorMessage));
-            Assert.IsTrue(query_result.ReturnValue.Count == 1);
-            from_query = query_result.ReturnValue[0];
-            Assert.IsTrue(permission.PermissionLevelId == from_query.PermissionLevelId);
-            Assert.IsTrue(permission.PermissionLevelName == from_query.PermissionLevelName);
-
-
             
-            bool_result = Simple_Update_By_Id(permissionConnector, 1, 2);
-            Assert.IsTrue(bool_result.ReturnValue);
-
-
-            //Query for original value to show it's not there
-            string_result = Simple_Query_By_Id(permissionConnector, 1, new List<Fields>());
-            //No error, but also no result
-            Assert.IsTrue(string.IsNullOrEmpty(string_result.ErrorMessage));
-            Assert.IsTrue(string.IsNullOrEmpty(string_result.ReturnValue));
-
-
-            //Update Id back to 1
-            bool_result = Simple_Update_By_Id(permissionConnector, 2, 1);
-            Assert.IsTrue(bool_result.ReturnValue);
-
-
-            //Query for update
-            string_result = Simple_Query_By_Id(permissionConnector, 1, new List<Fields>());
-            Assert.IsTrue(string.IsNullOrEmpty(string_result.ErrorMessage));
-            //Parse query and verify it matches original
-            query_result = Parse_Query(string_result.ReturnValue);
-            Assert.IsTrue(string.IsNullOrEmpty(query_result.ErrorMessage));
-            Assert.IsTrue(query_result.ReturnValue.Count == 1);
-            from_query = query_result.ReturnValue[0];
-            Assert.IsTrue(permission.PermissionLevelId == from_query.PermissionLevelId);
-            Assert.IsTrue(permission.PermissionLevelName == from_query.PermissionLevelName);
-
-
-            //Delete test record
-            bool_result = Simple_Delete_By_Id(permissionConnector, 1);
-            Assert.IsTrue(bool_result.ReturnValue, bool_result.ErrorMessage);
         }
         
-
-        private ResultPackage<bool> Simple_Insert(PermissionLevelsConnector connector, PermissionLevel permission)
-        {
-            return connector.Insert(permission.PermissionLevelId, permission.PermissionLevelName);
-        }
 
         private ResultPackage<string> Simple_Query_By_Id(PermissionLevelsConnector connector, long id, List<Fields> select_fields)
         {
@@ -98,50 +43,50 @@ namespace DBConnector.Tests
             return connector.Query(where_dict, select_fields);
         }
         
-        private ResultPackage<List<PermissionLevel>> Parse_Query(string query_result)
-        {
-            long long_out;
-            string trimmed_value;
-            List<string> rows = new List<string>();
-            char[] delimit_fields = new char[] { ',' };
-            string[] delimit_rows = new string[] { "\r\n" };
-            List<PermissionLevel> query_permission_list = new List<PermissionLevel>();
-            ResultPackage<List<PermissionLevel>> result = new ResultPackage<List<PermissionLevel>>();
+        //private ResultPackage<List<PermissionLevel>> Parse_Query(string query_result)
+        //{
+        //    long long_out;
+        //    string trimmed_value;
+        //    List<string> rows = new List<string>();
+        //    char[] delimit_fields = new char[] { ',' };
+        //    string[] delimit_rows = new string[] { "\r\n" };
+        //    List<PermissionLevel> query_permission_list = new List<PermissionLevel>();
+        //    ResultPackage<List<PermissionLevel>> result = new ResultPackage<List<PermissionLevel>>();
 
-            rows = query_result.Split(delimit_rows, StringSplitOptions.RemoveEmptyEntries).ToList();
-            foreach (string row in rows)
-            {
-                PermissionLevel permission = new PermissionLevel();
-                List<string> field_values = row.Split(delimit_fields).ToList();
+        //    rows = query_result.Split(delimit_rows, StringSplitOptions.RemoveEmptyEntries).ToList();
+        //    foreach (string row in rows)
+        //    {
+        //        PermissionLevel permission = new PermissionLevel();
+        //        List<string> field_values = row.Split(delimit_fields).ToList();
 
-                foreach (string field_value in field_values)
-                {
-                    if (field_value.Contains("PermissionLevelId::"))
-                    {
-                        trimmed_value = field_value.Replace("PermissionLevelId::", "");
-                        if (long.TryParse(trimmed_value, out long_out)) { permission.PermissionLevelId = long_out; }
-                        else
-                        {
-                            result.ErrorMessage = "Unable to parse PermissionLevelId";
-                            break;
-                        }
-                    }
-                    else if (field_value.Contains("PermissionLevelName::"))
-                    {
-                        permission.PermissionLevelName = field_value.Replace("PermissionLevelName::", "").Trim();
-                    }
-                }
+        //        foreach (string field_value in field_values)
+        //        {
+        //            if (field_value.Contains("PermissionLevelId::"))
+        //            {
+        //                trimmed_value = field_value.Replace("PermissionLevelId::", "");
+        //                if (long.TryParse(trimmed_value, out long_out)) { permission.PermissionLevelId = long_out; }
+        //                else
+        //                {
+        //                    result.ErrorMessage = "Unable to parse PermissionLevelId";
+        //                    break;
+        //                }
+        //            }
+        //            else if (field_value.Contains("PermissionLevelName::"))
+        //            {
+        //                permission.PermissionLevelName = field_value.Replace("PermissionLevelName::", "").Trim();
+        //            }
+        //        }
 
-                if (!string.IsNullOrEmpty(result.ErrorMessage))
-                    break;
+        //        if (!string.IsNullOrEmpty(result.ErrorMessage))
+        //            break;
 
-                query_permission_list.Add(permission);
-            }
+        //        query_permission_list.Add(permission);
+        //    }
 
-            result.ReturnValue = query_permission_list;
+        //    result.ReturnValue = query_permission_list;
 
-            return result;
-        }
+        //    return result;
+        //}
 
         private ResultPackage<bool> Simple_Update_By_Id(PermissionLevelsConnector connector, long old_id, long new_id)
         {
@@ -166,6 +111,5 @@ namespace DBConnector.Tests
 
             return connector.Delete(where_values);
         }
-        */
     }
 }
