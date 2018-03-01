5836cd12185018d01944839474a529b5d3c959ec
Kenneth Petry <kpetry@bresnan.net>
2018-03-01 12:40:39 -0700

    Added more DAO items to access/update the database, and added a function in the file manager to update the db.

--- Mobile/AugmentedEducationAR/app/src/main/java/com/augmentededucation/ar/augmentededucationar/FileManager.java
+++ Mobile/AugmentedEducationAR/app/src/main/java/com/augmentededucation/ar/augmentededucationar/FileManager.java
@@ -6,6 +6,9 @@ import android.content.Context;
 
 import com.augmentededucation.ar.augmentededucationar.WebAccess.WebAccessor;
 import com.augmentededucation.ar.augmentededucationar.db.AppDatabase;
+import com.augmentededucation.ar.augmentededucationar.db.entity.Model;
+
+import java.util.List;
 
 /**
  * Created by kpetr on 2/28/2018.
@@ -28,5 +31,15 @@ public class FileManager
 
 	public void downloadFile(String location) {
 
+	}
+
+	public void addModelToDatabase(Model model) {
+		List<Model> models = db.modelDao().loadModelWhereURL(model.url);
+		if (models.size() == 0) {
+			db.modelDao().insertModel(model);
+		}
+		else {
+			db.modelDao().updateModel(model.url, model.name, model.location);
+		}
 	}
 }
--- Mobile/AugmentedEducationAR/app/src/main/java/com/augmentededucation/ar/augmentededucationar/db/dao/ModelDao.java
+++ Mobile/AugmentedEducationAR/app/src/main/java/com/augmentededucation/ar/augmentededucationar/db/dao/ModelDao.java
@@ -4,6 +4,7 @@ import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
 
 import com.augmentededucation.ar.augmentededucationar.db.entity.Model;
 
@@ -19,6 +20,12 @@ import static android.arch.persistence.room.OnConflictStrategy.IGNORE;
 public interface ModelDao {
     @Query("SELECT * FROM Model")
     List<Model> loadAllModels();
+
+    @Query("SELECT * FROM Model WHERE Model.url = :url")
+    List<Model> loadModelWhereURL(String url);
+
+    @Query("UPDATE Model SET location = :newLocation, name = :newName WHERE Model.url = :url")
+    List<Model> updateModel(String url, String newName, String newLocation);
 
     @Insert(onConflict = IGNORE)
     void insertModel(Model model);
