64a1a8189873014fcb3e32f9c6413547bd6b712c
7180134 <daniel.hodgin@mines.sdsmt.edu>
2018-03-13 18:06:41 -0600

    rerouted start to go to the public content page

--- Website/ARFE/App_Start/RouteConfig.cs
+++ Website/ARFE/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace ARFE
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "PublicContent", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
