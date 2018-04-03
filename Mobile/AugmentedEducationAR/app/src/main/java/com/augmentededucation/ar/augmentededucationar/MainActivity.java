8f91e44ce0206786a332b6d4dbd6de3cca422415
Kenneth Petry <kpetry@bresnan.net>
2018-04-03 12:49:57 -0600

    Adding documentaiton to the MainActivity.

--- Mobile/AugmentedEducationAR/app/src/main/java/com/augmentededucation/ar/augmentededucationar/MainActivity.java
+++ Mobile/AugmentedEducationAR/app/src/main/java/com/augmentededucation/ar/augmentededucationar/MainActivity.java
@@ -17,16 +17,39 @@ import com.augmentededucation.ar.augmentededucationar.WebAccess.WebAccessor;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+/**
+ * The first activity when opening the app.  There is functionality to authenticate the user to view online content, and
+ * an offline mode for no online connectivity.
+ */
 public class MainActivity extends AppCompatActivity {
+
+    /**
+     * Username and Password stored in preferences so the user does not need to retype it in every time the app is loaded.
+     */
     private static final String USERNAME = "USERNAME";
     private static final String PASSWORD = "PASSWORD";
+
+    /**
+     * The text boxes for the username and password.
+     */
     private TextView username;
     private TextView password;
+
+    /**
+     * Eventually set as the value of the text boxes
+     */
     private String username_string = "";
     private String password_string = "";
 
+    /**
+     * Used to get an authentication token from the website.
+     */
     private WebAccessor accessor;
 
+    /**
+     * Creates the activity.  Initializes the username/password information if needed.
+     * @param savedInstanceState The saved state of recreating the activity
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -58,12 +81,24 @@ public class MainActivity extends AppCompatActivity {
         password.setText(password_string);
     }
 
+    /**
+     * Do not access the website, just continue without an authentication token.
+     * @param view The calling view (needed for as an onClick handler).
+     */
     public void continueOffline(View view) {
         Intent intent = new Intent(getBaseContext(), HomeActivity.class);
         intent.putExtra(getString(R.string.web_AuthToken), "");
         startActivity(intent);
     }
 
+    /**
+     * Authenticate with the website and move to the MainActivity with the auth token.
+     * Saves the username and password in the preferences if the remember me box is checked.
+     *
+     * TODO: Make better error statements/toasts
+     *
+     * @param v The calling view.
+     */
     public void onNext(View v) {
         SharedPreferences sharedPreferences = getPreferences(Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
@@ -79,33 +114,27 @@ public class MainActivity extends AppCompatActivity {
         editor.apply();
 
         accessor.authenticate(username.getText().toString(), password.getText().toString(),
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        try
+                response -> {
+                    try
+                    {
+                        if (response.get("success").toString().equals("True"))
                         {
-                            if (response.get("success").toString().equals("True"))
-                            {
-                                Intent intent = new Intent(getBaseContext(), HomeActivity.class);
-                                intent.putExtra(getString(R.string.web_AuthToken), response.getString("token"));
-                                startActivity(intent);
-                            }
-                            else {
-                                Toast.makeText(getBaseContext(), response.get("reason").toString(), Toast.LENGTH_SHORT).show();
-                            }
+                            Intent intent = new Intent(getBaseContext(), HomeActivity.class);
+                            intent.putExtra(getString(R.string.web_AuthToken), response.getString("token"));
+                            startActivity(intent);
                         }
-                        catch (JSONException ex) {
-                            //Toast.makeText(getBaseContext(), "Unable to authenticate", Toast.LENGTH_SHORT).show();
-                            Toast.makeText(getBaseContext(), response.toString(), Toast.LENGTH_SHORT).show();
+                        else {
+                            Toast.makeText(getBaseContext(), response.get("reason").toString(), Toast.LENGTH_SHORT).show();
                         }
+                    }
+                    catch (JSONException ex) {
+                        //Toast.makeText(getBaseContext(), "Unable to authenticate", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getBaseContext(), response.toString(), Toast.LENGTH_SHORT).show();
                     }
                 },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Toast.makeText(getBaseContext(), error.toString(), Toast.LENGTH_LONG).show();
-                        //Toast.makeText(getBaseContext(), "Unable to authenticate", Toast.LENGTH_LONG).show();
-                    }
+                error -> {
+                    Toast.makeText(getBaseContext(), error.toString(), Toast.LENGTH_LONG).show();
+                    //Toast.makeText(getBaseContext(), "Unable to authenticate", Toast.LENGTH_LONG).show();
                 }
         );
 
