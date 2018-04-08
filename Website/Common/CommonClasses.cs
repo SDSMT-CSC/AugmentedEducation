4d5e6ad0616b08c387c83181983c2eba70b5b7d8
Brady Shimp <Brady.Shimp@mines.sdsmt.edu>
2018-04-08 16:25:18 -0600

    Added call to blob manager to get blob info back with file names for UI display

---
+++ Website/Common/CommonClasses.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class FileUIInfo
+    {
+        #region Constructor
+
+        public FileUIInfo(string fileName = "", string author = "", string description = "", DateTime uploaded = new DateTime())
+        {
+            Author = author;
+            FileName = fileName;
+            Description = description;
+            UploadDate = uploaded;
+        }
+
+        #endregion
+
+
+        #region Properties
+
+        public string FileName { get; set; }
+        public string Author { get; set; }
+        public string Description { get; set; }
+        public DateTime UploadDate { get; set; }
+
+        #endregion
+    }
+}
