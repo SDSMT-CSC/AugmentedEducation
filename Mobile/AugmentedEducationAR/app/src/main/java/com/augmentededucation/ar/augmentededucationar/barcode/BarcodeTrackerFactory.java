55e4cdbfba645c054b74fecc334eab9ee27be317
Aaron Alphonsus <aaron.alphonsus@gmail.com>
2018-04-24 10:53:22 -0600

    Added more documentation.

--- Mobile/AugmentedEducationAR/app/src/main/java/com/augmentededucation/ar/augmentededucationar/barcode/BarcodeTrackerFactory.java
+++ Mobile/AugmentedEducationAR/app/src/main/java/com/augmentededucation/ar/augmentededucationar/barcode/BarcodeTrackerFactory.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.augmentededucation.ar.augmentededucationar.barcode;
 
 import android.content.Context;
@@ -10,6 +25,10 @@ import com.google.android.gms.vision.barcode.Barcode;
  * Created by 7296837 on 2/6/2018.
  */
 
+/**
+ * Factory for creating a tracker to be associated with a new barcode.  The multi-processor uses
+ * this factory to create barcode trackers as needed - one for each barcode.
+ */
 public class BarcodeTrackerFactory implements MultiProcessor.Factory<Barcode>{
     private Context mContext;
 
